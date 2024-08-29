599584d36d9c6a226121f241c7ad358dce45d4b0
Hakan Türkmen <hakannturkmen@gmail.com>
2024-08-29 21:34:31 +0300

    Initial Commit

---
+++ TruthFirm/TruthFirmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruthFirmApp.swift
+//  TruthFirm
+//
+//  Created by Hakan Türkmen on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TruthFirmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
